061529499f4a3d1a80533de3bc863eba3adec252
Pranitha220701 <70841166+Pranitha220701@users.noreply.github.com>
2024-03-04 12:56:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace VarDynamic
+{
+    public class VarDynamic
+    {
+        public static void Main(string[] args)
+        {
+            var a = 3;
+            dynamic b;
+            Console.WriteLine("Enter b value:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
